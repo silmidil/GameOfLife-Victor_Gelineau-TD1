d4799e65e8d42a0807d58f90e34e3a09d284a168
Silmidil <victor.gelineau@gmail.com>
2025-01-14 14:39:36 +0100

    Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='game_of_life',  
+    version='1.0',
+    author='Silmidil', 
+    license='MIT',
+    long_description=open('README.md').read(),  
+)
